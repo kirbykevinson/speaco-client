b50d800706097dca6df284532ebc85c9dd6e9ff9
kirbykevinson <kirbykevinson@outlook.com>
2021-03-31 16:14:10 +0700

    Add basic WebSocket handling code

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -13,9 +13,40 @@ class Chat {
 		
 		this.serverIp = null;
 		this.nickname = null;
+		
+		this.socket = null;
+	}
+	
+	error(message, error) {
+		alert("Error: " + message);
+		
+		this.close();
+	}
+	
+	open() {
+		this.elements.registration.hidden = true;
+		this.elements.chat.hidden = false;
+	}
+	close() {
+		if (this.socket) {
+			if (this.socket.readyState != WebSocket.CLOSED) {
+				this.socket.close();
+			}
+			
+			this.socket = null;
+		}
+		
+		this.elements.registration.hidden = false;
+		this.elements.chat.hidden = true;
 	}
 	
 	join() {
+		if (this.socket) {
+			this.error("already connected");
+			
+			return;
+		}
+		
 		this.serverIp =
 			this.elements.serverIp.value ||
 			this.elements.serverIp.placeholder;
@@ -23,16 +54,69 @@ class Chat {
 			this.elements.nickname.value ||
 			this.elements.nickname.placeholder;
 		
-		this.elements.registration.hidden = true;
-		this.elements.chat.hidden = false;
+		this.socket = new WebSocket("ws://" + this.serverIp);
+		
+		this.socket.addEventListener("error", (event) => {
+			this.error("couldn't connect to the server");
+		});
+		this.socket.addEventListener("close", (event) => {
+			this.close();
+		});
+		
+		this.socket.addEventListener("open", (event) => {
+			this.sendEvent("join", {
+				nickname: this.nickname
+			});
+		});
+		
+		this.socket.addEventListener("message", (event) => {
+			let parsedEvent = null;
+			
+			try {
+				parsedEvent = JSON.parse(event.data);
+			} catch (error) {
+				this.error("malformed event");
+				
+				return;
+			}
+			
+			if (!parsedEvent || typeof parsedEvent != "object") {
+				this.error("event is not an object");
+				
+				return;
+			}
+			
+			if (!("type" in parsedEvent)) {
+				this.error("event without a type");
+				
+				return;
+			}
+			
+			this.recieveEvent(parsedEvent);
+		});
+	}
+	
+	sendEvent(type, data) {
+		if (!data || typeof data != "object") {
+			throw new TypeError("event data must be an object");
+		}
+		
+		data.type = type;
+		
+		this.socket.send(JSON.stringify(data));
+	}
+	recieveEvent(event) {
+		switch (event.type) {
+		default:
+			this.error("illegal event type");
+			
+			return;
+		}
 	}
 	
 	sendMessage() {
-		this.recieveMessage(this.nickname, this.elements.message.value);
 		
-		this.elements.message.value = "";
 	}
-	
 	recieveMessage(sender, message) {
 		const container = document.createElement("p");
 		
